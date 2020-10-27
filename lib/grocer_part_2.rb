57c65aed905cca76e50d99073d6e4ff3f6829b31
Javaria Brascom <javariab17@gmail.com>
2020-10-27 15:48:33 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,12 +4,35 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+counter=0
+while counter < coupons.length
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+  couponed_item_name= "#{coupons[counter][:item]} W/COUPON "
+  cart_item_with_coupon= find_item_by_name_in_collection(couponed_item_name,cart)
+  if cart_item && cart_item[:count]>= coupons[counter][:num]
+    if cart_item_with_coupon
+      cart_item_with_coupon[:count]+= coupons[:num]
+cart_item[:count]-= coupons[:num]
+else cart_item_with_coupon = {
+  :item=> couponed_item_name,
+  :price=> coupons[counter][:cost] / coupons[counter][:num],
+  :count=> coupons[counter][:num],
+  :clearance => cart_item[:clearance]
+}
+
+cart << cart_item_with_coupon
+cart_item[:count] -= coupons[counter][:num]
+end
+counter += 1
+end
+cart
+end
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < cart.length 
+  cart[counter][:price] = {cart[counter][:price] - (cart[counterp[:price]])
 end
 
 def checkout(cart, coupons)
